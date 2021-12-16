8ae76c4ca5ce9b651a4546d047b9389ab4b5e1fa
Andy Bartkiv <andy.bartkiv@gmail.com>
2021-12-16 19:33:47 +0300

    replace BrowserRouter with HashRouter for GH deployment

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 const Error = () => {
     return (
         <div className="App">
-            <h4 style={{ color: 'orange' }}>Oops, there's nothing here!</h4>
+            <h4 style={{ color: 'orange' }}>Ooops, there's nothing here!</h4>
             <hr style = { {margin: '.5em 0', background: 'teal', border: 'none', height: '.05em'} }/>
             <Link to='/'>Go Back</Link>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import About from './components/About';
 import Error from './components/Error';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
-      <Route path="/" exact element = { <App/> }/>
+      <Route exact path="/" element = { <App/> }/>
       <Route path="/about" element = { <About/> }/>
       <Route path="*" element={ <Error/> }/>
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
